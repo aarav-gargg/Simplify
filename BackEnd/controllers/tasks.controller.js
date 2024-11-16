7daa08c8c022dd302cb248647a24f6d4e8813f95
aarav-gargg <aaravgarg975@gmail.com>
2024-11-16 22:28:36 +0530

    "Added two new controller functions and corresponding routes for updating task importance and completion status"

--- BackEnd/controllers/tasks.controller.js
+++ BackEnd/controllers/tasks.controller.js
@@ -87,6 +87,51 @@ export const updateTask = async (req,res) =>{
     }
 }
 
+export const updateImpTask = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const task = await Task.findById(id);
+
+        if (!task) {
+            return res.status(404).json({ message: "Task not found" });
+        }
+
+        task.important = !task.important;
+
+        await task.save();
+
+        return res.status(200).json({
+            message: "Task updated successfully",
+        });
+
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+};
+
+export const updateComTask = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const task = await Task.findById(id);
+
+        if (!task) {
+            return res.status(404).json({ message: "Task not found" });
+        }
+
+        task.complete = !task.complete;
+
+        await task.save();
+
+        return res.status(200).json({
+            message: "Task updated successfully",
+        });
+
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+};
 
 
 
--- BackEnd/routes/tasks.route.js
+++ BackEnd/routes/tasks.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createTask  , getTasks , deleteTask , updateTask} from "../controllers/tasks.controller.js";
+import { createTask  , getTasks , deleteTask , updateTask , updateImpTask , updateComTask} from "../controllers/tasks.controller.js";
 import { authenticate } from "../controllers/user.controller.js";
 
 const router = express.Router();
@@ -8,5 +8,7 @@ router.post("/create" , authenticate , createTask);
 router.get("/all" , authenticate , getTasks);
 router.delete("/delete-task/:id" , authenticate , deleteTask);
 router.put("/update/:id" , authenticate , updateTask)
+router.put("/updateImp/:id" , authenticate , updateImpTask)
+router.put("/updateCom/:id" , authenticate , updateComTask)
 
 export default router;
